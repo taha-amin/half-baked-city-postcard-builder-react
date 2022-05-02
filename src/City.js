20668b5b2b99a48f4f68379a2200e58bfd3a40d7
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-02 14:12:18 -0700

    finished writing city.js

--- src/City.js
+++ src/City.js
@@ -3,8 +3,11 @@ import React from 'react';
 export default function City(props) {
   return (
     <div className="city">
-      {/* this component should take 3 props: waterfrontId, skylineId, and castlId */}
+      {/* this component should take 3 props: waterfrontId, skylineId, and castleId */}
       {/* use these 3 ids in img tags load approriate images that live in the public directory */}
+      <img src={`/waterfront-${props.waterfrontId}.jpeg`} />
+      <img src={`/skyline-${props.skylineId}.jpeg`} />
+      <img src={`/castle-${props.castleId}.jpeg`} />
     </div>
   );
 }

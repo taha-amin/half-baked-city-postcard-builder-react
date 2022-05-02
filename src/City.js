c648a3e0345417252fb61b7e601050e2ff5b03d0
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-02 14:16:22 -0700

    refactored city.js

--- src/City.js
+++ src/City.js
@@ -4,10 +4,10 @@ export default function City(props) {
   return (
     <div className="city">
       {/* this component should take 3 props: waterfrontId, skylineId, and castleId */}
-      {/* use these 3 ids in img tags load approriate images that live in the public directory */}
-      <img src={`/waterfront-${props.waterfrontId}.jpeg`} />
-      <img src={`/skyline-${props.skylineId}.jpeg`} />
-      <img src={`/castle-${props.castleId}.jpeg`} />
+      {/* use these 3 ids in img tags load appropriate images that live in the public directory */}
+      <img src={`${props.waterfrontId}.jpeg`} />
+      <img src={`${props.skylineId}.jpeg`} />
+      <img src={`${props.castleId}.jpeg`} />
     </div>
   );
 }

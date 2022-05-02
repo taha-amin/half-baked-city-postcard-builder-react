a86e17be5214591bd59615b0351f7425ed50423b
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-02 12:08:50 -0700

    in slogan.js took in a slogan as a prop then rendered it

--- src/Slogan.js
+++ src/Slogan.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Slogan(props) {
   return (
-    <div className='slogan'>
+    <div className="slogan">
       {/* this component takes in a slogan as a prop. Render it out here however you like */}
+      {props.slogan}
     </div>
   );
 }
